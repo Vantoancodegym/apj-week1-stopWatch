d683ff8a7f0425763528938de3d28afb0367d951
vantoanftu93 <73921998+vantoanftu93@users.noreply.github.com>
2020-12-14 11:59:41 +0700

    count char of string

---
+++ src/StopWatch.java
@@ -0,0 +1,28 @@
+import java.util.Date;
+
+public class StopWatch {
+    private Date startTime;
+    private Date endTime;
+    StopWatch(){
+        this.startTime=new Date();
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void start(){
+        this.startTime=new Date();
+    }
+    public void stop(){
+        this.endTime=new Date();
+    }
+    public long getElapsedTime(){
+        return this.endTime.getTime()-this.startTime.getTime();
+    }
+
+}
